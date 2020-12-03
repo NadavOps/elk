fa5af71c49e01da1c76e34cc326480ddfd8a95d7
Nadav.Ops <ubuntu@LetsGo-2.localdomain>
2020-12-03 02:22:12 +0200

    changing structure, working over a better es template

--- assets/user_data_scripts/es_initial_master_node.sh
+++ assets/user_data_scripts/es_initial_master_node.sh
@@ -1,16 +1,50 @@
 #!/bin/bash
 #### changing hostname
-hostnamectl set-hostname master${index}.${domain}
+hostnamectl set-hostname ${es_cluster_name}-${es_node_description}${index}.${domain}
 
 #### installing java, installing elasticsearch and registering it to systemd
 sudo apt update -y
 sudo apt install openjdk-11-jdk -y
-elasticsearch_version=${elasticsearch_version}
+elasticsearch_version=${es_version}
 curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$elasticsearch_version-amd64.deb
 sudo dpkg -i elasticsearch-$elasticsearch_version-amd64.deb
 rm -rf elasticsearch-$elasticsearch_version-amd64.deb
 sudo /bin/systemctl daemon-reload
 sudo /bin/systemctl enable elasticsearch.service
+
+#### Query AWS internal zone and get 3 masters for ES discovery using file -> /etc/elasticsearch/unicast_hosts.txt
+apt install -y awscli jq
+for interval in {1..6}
+do
+    count=0
+    records_raw=$(aws route53 list-resource-record-sets --hosted-zone-id ${route53_es_zone_id} --query "ResourceRecordSets[?Type == 'A']")
+    records_names=$(echo $records_raw | jq -r .[].Name | rev | cut -c 2- | rev)
+    for record in $records_names
+    do
+        echo $record
+        cluster_name_prefix=$(echo $record | cut -d "-" -f1)
+        node_name_suffix=$(echo $record | cut -d "-" -f2)
+        if [[ $cluster_name_prefix == ${es_cluster_name} && $node_name_suffix == *"master"* ]]
+        then
+            count=$((count+1))
+        fi
+    done
+    if [[ $count -gt 2 ]]; then echo "found more than 2 masters"; break; fi
+    echo "didn't found more than 2 masters, sleeping 10, and retrying"
+    sleep 10
+done
+
+for record in $records_names
+do
+    cluster_name_prefix=$(echo $record | cut -d "-" -f1)
+    node_name_suffix=$(echo $record | cut -d "-" -f2)
+    if [[ $cluster_name_prefix == ${es_cluster_name} && $node_name_suffix == *"master"* ]]
+    then
+        echo "Adding $record to /etc/elasticsearch/unicast_hosts.txt"
+        echo $record >> /etc/elasticsearch/unicast_hosts.txt
+    fi
+    if [[ -f /etc/elasticsearch/unicast_hosts.txt && $(cat /etc/elasticsearch/unicast_hosts.txt | wc -l) -gt 2 ]]; then break; fi
+done
 
 #### configuring jvm.options to 2g heapsize, saving elasticsearch.yml defaults configuration, configuring elastic.yml
 LOCAL_IPV4=$(curl "http://169.254.169.254/latest/meta-data/local-ipv4")
@@ -20,12 +54,12 @@ cp /etc/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml.unu
 
 cat << EOF > /etc/elasticsearch/elasticsearch.yml
 ## Cluster
-cluster.name: ${elasticsearch_cluster_name}
+cluster.name: ${es_cluster_name}
 
 ## Node
-node.name: master${index}.${domain}
+node.name: ${es_cluster_name}-${es_node_description}${index}.${domain}
 
-node.roles: [ master ]
+node.roles: [ ${node_roles} ]
 # Add custom attributes to the node, for example: node.attr.rack: r1
 
 ## Paths, changing the defaults so reinstallation will not overwrite data
@@ -36,9 +70,15 @@ path.logs: /etc/elasticsearch/state_elasticsearch_logs
 network.host: ["localhost", "$LOCAL_IPV4"]
 
 ## Cluster Bootstrap
-discovery.seed_hosts: [${discovery_seed_hosts}]
-cluster.initial_master_nodes: [${masters_dns_records}]
+discovery.seed_providers: file
 EOF
+
+### Adding to elasticsearch the bootstrap configuration if it is a bootstrap run
+if [[ ${es_node_description} == *"initial"* ]]
+then
+    echo "The node is an initial bootstrap node, adding relevant configuration"
+    echo "cluster.initial_master_nodes: [${initial_masters_dns_records}]" >> /etc/elasticsearch/elasticsearch.yml
+fi
 
 #### Verify elasticsearch user have the permissions, and starting elasticsearch
 chown -R elasticsearch:elasticsearch /etc/elasticsearch
@@ -61,6 +101,10 @@ done
 #### Sets dynamically the required number of master eligible nodes by the cluster for elections
 #### The rule is N/2 + 1
 sleep 20
+
+# add for line in $(curl -s "localhost:9200/_cat/nodes?v&h=node.role")
+
+
 master_eligible_nodes=$(expr $(curl -s "localhost:9200/_cat/nodes?v&h=master" | wc -l) - 1)
 let "required_master_eligible_nodes_for_elections = $master_eligible_nodes/2 + 1"
 curl -X PUT localhost:9200/_cluster/settings -H "Content-Type: application/json" -d'
